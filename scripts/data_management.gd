4dd711aaf13442d00153ddb7d05f16d0ef90aff0
DevStulk <devstulk@gmail.com>
2024-06-27 09:43:26 -0300

    fix: change file of save common to encrypted file for test.

--- scripts/data_management.gd
+++ scripts/data_management.gd
@@ -1,25 +1,38 @@
 extends Node2D
 
-var save_path: String = 'res://best_score.cfg'
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# If not exist file create.
-	if !FileAccess.file_exists('best_score.cfg'):
-		var config = ConfigFile.new()
-		config.save(save_path)
+const password: String = '1J3HRTFUI6O0';
+var file_path: String = 'user://save.dat'
 
 # Save data in file.
 func _save_data(score):
-	var config = ConfigFile.new()
-	config.set_value('score', 'best_score', score)
-	config.save(save_path)
+	var file = FileAccess.open_encrypted_with_pass(file_path, FileAccess.WRITE, password)
+	if file == null:
+		printerr('Error code: %d when try open file: %s' % [FileAccess.get_open_error(), file_path])
+		print(FileAccess.get_open_error())
+		return
+	
+	var data = {
+		'best_score': score,
+	}
+	
+	var json = JSON.stringify(data, '\t')
+	file.store_string(json)
+	file.close()
 
 # Load data in file.
 func _load_data():
-	var config = ConfigFile.new()
-	var err = config.load(save_path)
-	if err != OK:
+	var file = FileAccess.open_encrypted_with_pass(file_path, FileAccess.READ, password)
+	if file == null:
+		return 0 # Rtruen 0 because non-exist best score.
+		file.close()
+	
+	var content = file.get_as_text()
+	file.close()
+	
+	var data = JSON.parse_string(content)
+	if data == null:
+		printerr('Cannot parse as a json: (%s)' % [file_path, content])
 		return
 	
-	return config.get_value('score', 'best_score', 0)
+	return data['best_score'] # return best score.
+	
